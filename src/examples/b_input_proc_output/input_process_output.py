949cfa4f8f42c286825e484054241cfea151247b
ecarrillo22 <eric.carrillo@g.austincc.edu>
2025-02-06 01:28:36 +0000

    int and floats

--- src/examples/b_input_proc_output/input_process_output.py
+++ src/examples/b_input_proc_output/input_process_output.py
@@ -5,3 +5,19 @@ def display_output():
 def test_config():
     return True
 
+def use_int_type():
+    num = 10 #create and assign a value to a variable
+    print(num)
+
+    num = num + 5 #reassign a value to the variable
+    print(num)
+
+def use_float_type():
+    num = 10.99
+    print(num)
+
+    num = 99.99
+    print(num)
+
+    num = num + 11.5
+    print(num)
