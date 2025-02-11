d02571ecb2c80bedb63bb2cb678504b0aa5ecd47
ecarrillo22 <eric.carrillo@g.austincc.edu>
2025-02-11 02:34:52 +0000

    hw2

--- src/examples/c_decisions/decisions.py
+++ src/examples/c_decisions/decisions.py
@@ -4,4 +4,5 @@ def test_config():
 def compare_numbers_equality(num1, num2):
 
     result = num1 == num2 #== means compare ----- = means assign a value to a variable
+    
     return result
--- src/homework/c_decisions/decisions.py
+++ src/homework/c_decisions/decisions.py
@@ -16,6 +16,3 @@ def get_faculty_rating(ratio):
         return 'Needs Improvement'
     elif (ratio <= 59):
         return 'Unacceptable'
-    else:
-        print("Ratio is invalid.")
-    
