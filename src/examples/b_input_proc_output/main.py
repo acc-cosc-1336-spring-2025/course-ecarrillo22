6ea2a89848262e47485199d6b18349c309eb1904
ecarrillo22 <eric.carrillo@g.austincc.edu>
2025-02-11 01:19:34 +0000

    input string to float

--- src/examples/b_input_proc_output/main.py
+++ src/examples/b_input_proc_output/main.py
@@ -4,7 +4,7 @@ def main():
     num1 = input("Enter a number: ") #Python treats input as a string(sequence of characters)
     num2 = input("Enter a number: ")
 
-    result = input_process_output.add_numbers(int(num1), int(num2)) #convert num1, num2 to int
+    result = input_process_output.add_numbers(float(num1), float(num2)) #convert num1, num2 to int
     print(result)
 
 main() #runs the code on line 3
--- src/homework/c_decisions/main.py
+++ src/homework/c_decisions/main.py
@@ -3,11 +3,11 @@ import decisions
 
 def main():
     #prompts user for options
-    options = float(input ('Enter an option: '))
-    total = float(input ('Enter a total: '))
+    options = input ('Enter an option: ')
+    total = input ('Enter a total: ')
 
     #creates a result variable from options ratio function
-    result = decisions.get_options_ratio(options, total)
+    result = decisions.get_options_ratio(float(options), float(total))
 
     #displays the rating 
     print(f'The rating is {decisions.get_faculty_rating(result)}')
