b14131560c3a28500b2be79e8128694645189dfc
ecarrillo22 <eric.carrillo@g.austincc.edu>
2025-02-20 01:59:23 +0000

    run

--- src/examples/d_repetition/main.py
+++ src/examples/d_repetition/main.py
@@ -1,7 +1,7 @@
 import repetition
 
 def main():
-    result = repetition.multiplication_table_with_while(10, 10)
+    result = repetition.multiplication_table_for(2, 2)
     
 #run the main function
 main()
