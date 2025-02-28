e9765578a4abe9cf7ab772123cfe34379e8dde39
ecarrillo22 <eric.carrillo@g.austincc.edu>
2025-02-28 01:12:06 +0000

    HW 4

--- src/homework/e_functions/main.py
+++ src/homework/e_functions/main.py
@@ -1,1 +1,15 @@
 #
+import value_return, void_func
+
+def main():
+
+    void_func.welcome_message()
+    epoch_seconds = int(input("Enter an epoch time: "))
+
+    hours = value_return.get_hour(epoch_seconds)
+    minutes = value_return.get_minutes(epoch_seconds)
+    seconds = value_return.get_seconds(epoch_seconds)
+
+    print(f"The time is {hours:02}:{minutes:02}:{seconds:02}")
+
+main()
